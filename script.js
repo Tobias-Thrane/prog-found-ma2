3c60a22495fd2b66d2faf900de8a53dedf8f0373
Tobias Thrane <tkthrane_93@hotmail.com>
2020-03-14 10:55:05 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// question 1
+//var petArray = ["cat", "cow", "dog"];
+
+//question 2
+//console.log(petArray[1]);
+
+//question 3
+//petArray.push("sheep");
+
+//question 4
+//var n = petArray.lenght;
+
+//question 5
+//var catObject = {
+//    name: "Snow",
+//    colour: "White",
+//    age: 4,
+//}
+
+//question 6
+//console.log(catObject.age);
+
+//question 7
+//var catArray = [
+//    {
+//       name: "Snow",
+//       colour: "White",
+//       age: 4
+//    }
+//];
+
+//console.log(catArray);
+
+//question 8
+//for(let cat of catArray) {
+//    console.log(cat);
+//}
+
+//question 9
+//function logToConsole(name) {
+//    var animal = name;
+//    console.log(animal);
+//}
+//
+////question 10
+//logToConsole("donkey");
